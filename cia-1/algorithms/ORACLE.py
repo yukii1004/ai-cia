14f3b690451792c67fc64f2035cded29bb08ba47
yukii1004 <nitinkarthick1909@gmail.com>
2024-10-20 22:45:25 +0530

    finished Code for ORACLE algorithm

--- cia-1/algorithms/ORACLE.py
+++ cia-1/algorithms/ORACLE.py
@@ -1,2 +1,44 @@
-def OR(maze, start, end):
-    pass
+from collections import deque
+
+class OR:
+    def __init__(self, maze, start, end):
+        self.maze = maze
+        self.start = start
+        self.end = end
+        self.rows = len(maze)
+        self.cols = len(maze[0])
+        self.optimal_path = self.find_optimal_path()
+
+    def is_valid(self, pos):
+        return 0 <= pos[0] < self.rows and 0 <= pos[1] < self.cols and self.maze[pos[0]][pos[1]] == 0
+
+    def get_neighbors(self, pos):
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        return [(pos[0] + d[0], pos[1] + d[1]) for d in directions if self.is_valid((pos[0] + d[0], pos[1] + d[1]))]
+
+    def find_optimal_path(self):
+        queue = deque([(self.start, [self.start])])
+        visited = set([self.start])
+
+        while queue:
+            current, path = queue.popleft()
+
+            if current == self.end:
+                return path
+
+            for neighbor in self.get_neighbors(current):
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    new_path = path + [neighbor]
+                    queue.append((neighbor, new_path))
+
+        return []  # No path found
+
+    def solve(self):
+        visited = set()
+        for step in self.optimal_path:
+            visited.add(step)
+            yield self.optimal_path[:self.optimal_path.index(step) + 1], visited, step == self.end
+
+        if not self.optimal_path:
+            yield [], visited, False
