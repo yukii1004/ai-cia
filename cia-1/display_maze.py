efeb4e66b2df9bde92cd4923d45bf2a3457d870c
yukii1004 <nitinkarthick1909@gmail.com>
2024-10-20 13:39:23 +0530

    Updated the disaply maze file

--- cia-1/display_maze.py
+++ cia-1/display_maze.py
@@ -1,10 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def load_maze_from_txt(filename='maze.txt'):
-    return np.loadtxt(filename, dtype=int)
-
-maze = load_maze_from_txt('./cia-1/maze.txt')
+maze = np.loadtxt('./cia-1/maze.txt', dtype=int)
 
 def display_maze(maze):
     fig = plt.figure(figsize=(10, 10))
